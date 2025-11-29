4cf7cf4237c9925d1a2766053880223806f8c882
ari <pixelatedariabusiness@gmail.com>
2025-11-29 15:57:27 +0000

    adasdsda

---
+++ modding/js/blaze.js
@@ -0,0 +1,55 @@
+window.onload = () => {
+  const audio = document.getElementById("holyshitthisisloud");
+
+  fetch("https://api-v2.pixelatedaria.space/modding/blaze/bannedusers")
+    .then(res => res.json())
+    .then(data => {
+      const display = document.getElementById("crash");
+      const users = data.bannedUsers;
+      display.innerHTML = "";
+
+      users.forEach(user => {
+        const name = user.username || "(no username)";
+
+        const div = document.createElement("div");
+        div.className = "banned-user";
+        div.innerHTML = `
+          <p><strong>ID:</strong> ${user.id}</p>
+          <p><strong>Name:</strong> ${name}</p>
+          <p><strong>Reason:</strong> ${user.reason}</p>
+          <hr>
+        `;
+        display.appendChild(div);
+      });
+    })
+    .catch(err => {
+      display.textContent = "Failed to load crash users";
+      console.error(err);
+    });
+
+    fetch("https://api-v2.pixelatedaria.space/modding/blaze/staff")
+    .then(res => res.json())
+    .then(data => {
+      const display = document.getElementById("staff");
+      const users = data.staff;
+      display.innerHTML = "";
+
+      users.forEach(user => {
+        const name = user.username || "(no username)";
+
+        const div = document.createElement("div");
+        div.className = "staff-user";
+        div.innerHTML = `
+          <p><strong>ID:</strong> ${user.id}</p>
+          <p><strong>Name:</strong> ${name}</p>
+          <p><strong>Role:</strong> ${user.role}</p>
+          <hr>
+        `;
+        display.appendChild(div);
+      });
+    })
+    .catch(err => {
+      display.textContent = "Failed to load staff users";
+      console.error(err);
+    });
+};
